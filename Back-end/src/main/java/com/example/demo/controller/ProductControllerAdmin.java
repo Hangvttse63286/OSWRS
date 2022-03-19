70b70e99f45ea59395d09627224fb6b6e4bb83ed
Nhinguyen <74651366+nhinguyen1402@users.noreply.github.com>
2022-03-20 02:16:41 +0700

    Revert "fix build"
    This reverts commit a6835f6b88260a30b46a2ac5a1a3b7546cb9364b.

--- Back-end/src/main/java/com/example/demo/controller/ProductControllerAdmin.java
+++ Back-end/src/main/java/com/example/demo/controller/ProductControllerAdmin.java
@@ -1,47 +1,70 @@
 package com.example.demo.controller;
 
+import java.io.File;
+import java.net.HttpURLConnection;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
+import javax.net.ssl.HttpsURLConnection;
+
+import org.hibernate.query.criteria.internal.expression.function.AggregationFunction.COUNT;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-
 import com.example.demo.common.Helper;
+import com.example.demo.config.FileUploadUtil;
+import com.example.demo.entity.Category;
+import com.example.demo.entity.ImgurData;
 import com.example.demo.entity.ImgurResponse;
 import com.example.demo.entity.Products;
+import com.example.demo.payload.CategoryDTO;
 import com.example.demo.payload.ProductCreateDTO;
+import com.example.demo.payload.ProductIncludeSkuDTO;
+import com.example.demo.payload.ProductListDTO;
 import com.example.demo.payload.ProductDTO;
 import com.example.demo.payload.ProductImageDTO;
-import com.example.demo.payload.ProductListDTO;
+import com.example.demo.payload.ProductIncludeImageDTO;
+import com.example.demo.repository.ProductRepository;
+
+import com.example.demo.payload.ProductSkuDTO;
+import com.example.demo.repository.ProductRepository;
+import com.example.demo.service.ProductImageService;
 import com.example.demo.service.ProductService;
+import com.google.gson.Gson;
+
+import lombok.experimental.var;
 
 
 
 @RestController
 @RequestMapping("/api/product/admin/")
 public class ProductControllerAdmin {
-
+	
 	@Autowired
 	private ModelMapper modelMapper;
-
+	
 	private final ProductService productService;
 	//private final ProductImageService productImageService;
-
+	
 	public ProductControllerAdmin(ProductService productService) {
 		super();
 		this.productService= productService;
 	}
-
+	
 	//ok
 	@RequestMapping(value = "/listProduct", method = RequestMethod.GET)
 	public ResponseEntity<?> listAllProducts(){
@@ -50,7 +73,7 @@ public class ProductControllerAdmin {
 		else
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 	}
-
+	
 	//ok
 	@RequestMapping(value = "/listProductIncludeImage", method = RequestMethod.GET)
 	public ResponseEntity<?> listAllProductIncludeImage(){
@@ -59,16 +82,16 @@ public class ProductControllerAdmin {
 		else
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 	}
-
+	
 	//ok
 	@RequestMapping(value = "/getProductById/{id}", method = RequestMethod.GET)
 	public ResponseEntity<ProductListDTO> getProductById(@PathVariable(name = "id") String id) {
-		if(productService.getProductById(id) != null)
+		if(productService.getProductById(id) != null) 
 			return new ResponseEntity<>(productService.getProductByIdAdmin(id), HttpStatus.OK);
 		else
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 	}
-
+	
 	//ok
 	@RequestMapping(value = "/deleteProductById/{id}", method = RequestMethod.DELETE)
 	public ResponseEntity<?> deleteProductById(@PathVariable(name = "id") String id) {
@@ -80,7 +103,7 @@ public class ProductControllerAdmin {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
 	}
-
+	
 	@RequestMapping(value = "/updateProductById/{id}", method = RequestMethod.PUT)
 	public ResponseEntity<ProductDTO> updateProductById(@PathVariable(name = "id") String id, @RequestBody ProductDTO productDTO) {
 		if(productService.getProductById(id) != null) {
@@ -90,7 +113,7 @@ public class ProductControllerAdmin {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
 	}
-
+	
 	@RequestMapping(value = "/createProduct", method = RequestMethod.POST)
 	public ResponseEntity<?> createProduct(@RequestBody ProductDTO productRequest) {
 		if(productService.createProduct(productRequest) == null) {
@@ -100,33 +123,33 @@ public class ProductControllerAdmin {
 			return new ResponseEntity<>(HttpStatus.OK);
 		}
 	}
-
-	@RequestMapping(value = "/createProductAll", method = RequestMethod.POST,
+	
+	@RequestMapping(value = "/createProductAll", method = RequestMethod.POST, 
 			consumes = { MediaType.MULTIPART_FORM_DATA_VALUE })
 	public ResponseEntity<ProductListDTO> createProductncludeImage(
 			@RequestPart("fileImage") MultipartFile[] multipartFile, ProductCreateDTO productRequest) throws Exception {
-
+		
 		List<ProductImageDTO> listImageDTOs= new ArrayList<ProductImageDTO>();
-
+		
 		Helper helper = new Helper();
 
 		for(MultipartFile multi: multipartFile) {
-
+			
 			ImgurResponse res = helper.getDataImgurResponse(multi);
-
+			
 			ProductImageDTO productImageDTO= new ProductImageDTO();
-
+			
 			productImageDTO.setUrl(res.getData().getLink());
-
+			
 			productImageDTO.setName(multi.getOriginalFilename());
-
+			
 			listImageDTOs.add(productImageDTO);
 		}
-
+		
 		productRequest.setProductImage(listImageDTOs);
-
-		Products savedProductIncludeImageDTO = productService.createProductAll(productRequest);
-
+	
+		Products savedProductIncludeImageDTO = productService.createProductAll(productRequest);	
+		
 		return new ResponseEntity<ProductListDTO>(HttpStatus.OK);
 	}
 
