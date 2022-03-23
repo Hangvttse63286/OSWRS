463b4be3dd2e4e270bed4945d14f0313549ab255
Hangvttse63286 <46786755+Hangvttse63286@users.noreply.github.com>
2022-03-24 04:42:58 +0700

    remove annotation @data on product

--- Back-end/src/main/java/com/example/demo/entity/Product.java
+++ Back-end/src/main/java/com/example/demo/entity/Product.java
@@ -3,7 +3,6 @@ package com.example.demo.entity;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
@@ -15,23 +14,15 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
-import org.hibernate.annotations.LazyCollection;
-import org.hibernate.annotations.LazyCollectionOption;
-
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
-@Data
 @Entity
 @Table(name = "products", uniqueConstraints = {@UniqueConstraint(columnNames = {"id"})})
 public class Product {
