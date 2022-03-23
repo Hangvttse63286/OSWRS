631c4ec8315aa9965785d5979c0d5c1ba01c4970
Hangvttse63286 <46786755+Hangvttse63286@users.noreply.github.com>
2022-03-23 22:12:37 +0700

    fix product

--- Back-end/src/main/java/com/example/demo/controller/ProductControllerUser.java
+++ Back-end/src/main/java/com/example/demo/controller/ProductControllerUser.java
@@ -1,26 +1,18 @@
 package com.example.demo.controller;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.entity.Category;
-import com.example.demo.entity.Products;
-import com.example.demo.payload.CategoryDTO;
 import com.example.demo.payload.ProductIncludeSkuDTO;
 import com.example.demo.payload.ProductListDTO;
-import com.example.demo.payload.ProductSkuDTO;
-import com.example.demo.service.ProductImageService;
 import com.example.demo.service.ProductService;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/product")
 public class ProductControllerUser {
@@ -36,7 +28,7 @@ public class ProductControllerUser {
 	//ok
 	@RequestMapping(value = "/listProduct", method = RequestMethod.GET)
 	public ResponseEntity<?> listAllProducts(){
-		if(productService.listAllProducts() != null)
+		if(!productService.listAllProducts().isEmpty())
 			return new ResponseEntity<>(productService.listAllProducts(), HttpStatus.OK);
 		else
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
@@ -45,7 +37,7 @@ public class ProductControllerUser {
 	//ok
 	@RequestMapping(value = "/listAllProductIncludeImage", method = RequestMethod.GET)
 	public ResponseEntity<?> listAllProductIncludeImage(){
-		if(productService.listAllProducts() != null)
+		if(!productService.listAllProducts().isEmpty())
 			return new ResponseEntity<>(productService.listAllProductIncludeImage(), HttpStatus.OK);
 		else
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
