f9c568801b76816811e5ea5304798f21142e231f
Hangvttse63286 <46786755+Hangvttse63286@users.noreply.github.com>
2022-03-29 19:47:46 +0700

    update recommend

--- Back-end/src/main/java/com/example/demo/repository/RecommendationRepository.java
+++ Back-end/src/main/java/com/example/demo/repository/RecommendationRepository.java
@@ -2,11 +2,13 @@ package com.example.demo.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.entity.Recommendation;
 import com.example.demo.entity.User;
 
 public interface RecommendationRepository extends JpaRepository<Recommendation, Long>{
+	@EntityGraph(attributePaths = { "user", "order", "product" })
 	List<Recommendation> findByUser(User user);
 }
