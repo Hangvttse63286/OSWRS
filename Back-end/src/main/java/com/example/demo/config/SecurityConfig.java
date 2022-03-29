e1eb7b57f90c6b855ee26c9cf4eeb9de715de224
Hangvttse63286 <46786755+Hangvttse63286@users.noreply.github.com>
2022-03-29 11:24:21 +0700

    fix cors policy bugs

--- Back-end/src/main/java/com/example/demo/config/SecurityConfig.java
+++ Back-end/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.demo.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +16,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.example.demo.service.CustomUserDetailsService;
 
@@ -24,10 +29,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private CustomUserDetailsService userDetailsService;
-    
+
     @Autowired
     private AuthEntryPointJwtConfig unauthorizedHandler;
-    
+
     @Bean
     public AuthTokenFilter authenticationJwtTokenFilter() {
     	return new AuthTokenFilter();
@@ -37,7 +42,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
-    
+
 //    @Override
 //    public void configure(WebSecurity web) throws Exception {
 //        web.ignoring().antMatchers("/v2/api-docs",
@@ -47,21 +52,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //                                   "/swagger-ui.html",
 //                                   "/webjars/**");
 //    }
-    
+
     private static final String[] AUTH_WHITELIST = {
             // -- swagger ui
-            "/v2/api-docs", 
-            "/swagger-resources/**", 
+            "/v2/api-docs",
+            "/swagger-resources/**",
             "/configuration/ui",
-            "/configuration/security", 
+            "/configuration/security",
             "/swagger-ui.html",
             "/swagger-ui.html#/**",
             "/webjars/**"
     };
-    
+
     private static final String[] ROLE_LIST = {
-            "USER", 
-            "STAFF", 
+            "USER",
+            "STAFF",
             "ADMIN"
     };
 
@@ -82,7 +87,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .httpBasic();
-        
+
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
@@ -96,6 +101,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
+        configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     //    @Override
