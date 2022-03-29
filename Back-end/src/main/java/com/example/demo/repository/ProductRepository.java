f06637816f8e95eeb1728185a8263d25f4832d59
Hangvttse63286 <46786755+Hangvttse63286@users.noreply.github.com>
2022-03-29 11:10:54 +0700

    fix hibernate create too many query

--- Back-end/src/main/java/com/example/demo/repository/ProductRepository.java
+++ Back-end/src/main/java/com/example/demo/repository/ProductRepository.java
@@ -3,6 +3,7 @@ package com.example.demo.repository;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -16,12 +17,17 @@ public interface ProductRepository extends JpaRepository<Product, String>{
 //	Optional<Product> findBySearch_word(String word);
 //	Boolean existsByProduct_id(String id);
 //	Boolean existsByProduct_name(String name);
+	@EntityGraph(attributePaths = { "categories", "productSKUs", "product_Image" })
 	List<Product> findAll();
 	//void deleteByProduct_id(String id);
 
 	//@Query(value = "select * FROM products where categories.id = ?1" , nativeQuery = true)
+	@EntityGraph(attributePaths = { "categories", "productSKUs", "product_Image" })
 	List<Product> findByCategoriesId(Long id);
 
+	@EntityGraph(attributePaths = { "categories", "productSKUs", "product_Image" })
 	List<Product> findByProductSKUsId(Long id);
+
+	@EntityGraph(attributePaths = { "categories", "productSKUs", "product_Image" })
 	List<Product> findAllByOrderBySoldDesc();
 }
