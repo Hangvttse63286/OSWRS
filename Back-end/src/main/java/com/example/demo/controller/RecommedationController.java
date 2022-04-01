69ef463433680c4f0ae9d249d06af4156d6e5f4d
Hangvttse63286 <46786755+Hangvttse63286@users.noreply.github.com>
2022-04-01 13:42:52 +0700

    update recommend api

--- Back-end/src/main/java/com/example/demo/controller/RecommedationController.java
+++ Back-end/src/main/java/com/example/demo/controller/RecommedationController.java
@@ -1,10 +1,21 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -15,6 +26,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.example.demo.payload.ProductRecommendationResponse;
 import com.example.demo.payload.RecommendationSaveRequest;
@@ -28,12 +40,26 @@ public class RecommedationController {
 	@Autowired
 	private RecommendationService recommendationService;
 
+	@Autowired
+	private RestTemplate restTemplate;
+
 	@GetMapping("/send_all")
 	public ResponseEntity<?> sendImageUrl() {
 		List<String> imageUrlList = recommendationService.getAllImage();
-		if (!imageUrlList.isEmpty())
-			return new ResponseEntity<>(imageUrlList, HttpStatus.OK);
-		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		if (!imageUrlList.isEmpty()) {
+			String URL = "";
+    		HttpHeaders headers = new HttpHeaders();
+    	    headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+    	    headers.setContentType(MediaType.APPLICATION_JSON);
+    	    HttpEntity<Object> entity = new HttpEntity<Object>(imageUrlList, headers);
+    		restTemplate.exchange(
+    			    URL,
+    			    HttpMethod.GET,
+    			    entity,
+    			    Void.class);
+    		return new ResponseEntity<>("Send successfully!", HttpStatus.OK);
+		}
+		return new ResponseEntity<>("No image found", HttpStatus.NOT_FOUND);
 	}
 
 	@GetMapping("/list")
@@ -48,11 +74,45 @@ public class RecommedationController {
 		return new ResponseEntity<>(productList, HttpStatus.OK);
 	}
 
-	@PostMapping("/save")
-	public ResponseEntity<?> getRecommendedProducts(@RequestBody RecommendationSaveRequest recommendationSaveRequest) {
-		int result = recommendationService.saveRecommendedProducts(recommendationSaveRequest.getImageUrlList(), recommendationSaveRequest.getUsername());
-		if (result == 0)
-			return new ResponseEntity<>("Error: User not found!", HttpStatus.NOT_FOUND);
-		return new ResponseEntity<>("Save successfully!", HttpStatus.OK);
+	@GetMapping("/save")
+	public ResponseEntity<?> saveRecommendedProducts() {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    	if (!(authentication instanceof AnonymousAuthenticationToken)) {
+    		UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+    		String URL = "https://my-recommendation-api.herokuapp.com/recommend";
+    		HttpHeaders headers = new HttpHeaders();
+    	    headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+    	    headers.setContentType(MediaType.APPLICATION_JSON);
+    	    HttpEntity<List<String>> entity = new HttpEntity<List<String>>(recommendationService.getLatestBoughtImagesByUser(userDetails.getUsername()), headers);
+    	    ResponseEntity<String> response = restTemplate.postForEntity(URL, entity, String.class);
+    	    String stringResult = response.getBody().replace(" ", "");
+    		List<String> imageReturnList = Arrays.asList(stringResult.substring(1, stringResult.length() - 2).replace("'", "").split(","));
+    		int result = recommendationService.saveRecommendedProducts(imageReturnList, userDetails.getUsername());
+    		if (result == 0)
+    			return new ResponseEntity<>("Error: User not found!", HttpStatus.NOT_FOUND);
+    		return new ResponseEntity<>("Save successfully!", HttpStatus.OK);
+    	}
+		return new ResponseEntity<>("Error: Please log in first!", HttpStatus.UNAUTHORIZED);
 	}
+
+	@GetMapping("/get")
+	public ResponseEntity<?> getLatestBoughtImagesByUser() {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    	if (!(authentication instanceof AnonymousAuthenticationToken)) {
+    		UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+
+    		List<String> imageList = recommendationService.getLatestBoughtImagesByUser(userDetails.getUsername());
+    		String URL = "https://my-recommendation-api.herokuapp.com/recommend";
+    		HttpHeaders headers = new HttpHeaders();
+    	    headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+    	    headers.setContentType(MediaType.APPLICATION_JSON);
+    	    HttpEntity<List<String>> entity = new HttpEntity<List<String>>(imageList, headers);
+    	    ResponseEntity<String> response = restTemplate.postForEntity(URL, entity, String.class);
+    		String stringResult = response.getBody().replace(" ", "");
+    		List<String> imageReturnList = Arrays.asList(stringResult.substring(1, stringResult.length() - 2).replace("'", "").split(","));
+    		return new ResponseEntity<>(imageReturnList, HttpStatus.OK);
+    	}
+		return new ResponseEntity<>("Error: Please log in first!", HttpStatus.UNAUTHORIZED);
+	}
+
 }
