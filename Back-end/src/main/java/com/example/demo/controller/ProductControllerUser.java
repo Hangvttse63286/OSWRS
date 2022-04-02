eec4add1e5c7f09657be2638213d5d605899150f
Hangvttse63286 <46786755+Hangvttse63286@users.noreply.github.com>
2022-04-02 14:27:49 +0700

    add product list feature

--- Back-end/src/main/java/com/example/demo/controller/ProductControllerUser.java
+++ Back-end/src/main/java/com/example/demo/controller/ProductControllerUser.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,10 +24,9 @@ public class ProductControllerUser {
 
 	private final ProductService productService;
 
-
 	public ProductControllerUser(ProductService productService) {
 		super();
-		this.productService= productService;
+		this.productService = productService;
 	}
 
 //	//ok
@@ -39,30 +39,56 @@ public class ProductControllerUser {
 //			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 //	}
 
-	//ok
-		@RequestMapping(value = "/listAllProductIncludeImage", method = RequestMethod.GET)
-		public ResponseEntity<?> listAllProductIncludeImage(){
-			List<ProductIncludeImageDTO> productList= productService.listAllProductIncludeImage();
-			if(!productList.isEmpty()) 
-				return new ResponseEntity<>(productList, HttpStatus.OK);
+	// ok
+	@RequestMapping(value = "/listAllProductIncludeImage", method = RequestMethod.GET)
+	public ResponseEntity<?> listAllProducts() {
+		List<ProductIncludeImageDTO> productList = productService.listAllProductIncludeImage();
+		if (!productList.isEmpty())
+			return new ResponseEntity<>(productList, HttpStatus.OK);
+		else
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	}
+
+	@RequestMapping(value = "/web/listAllProductIncludeImage", method = RequestMethod.GET)
+	public ResponseEntity<?> listAllProductWeb(@RequestBody int fromIndex) {
+		List<ProductIncludeImageDTO> productList = productService.listAllProductIncludeImage();
+		int toIndex = fromIndex + 20;
+		if (!productList.isEmpty()) {
+			if (toIndex < productList.size())
+				return new ResponseEntity<>(productList.subList(fromIndex, toIndex), HttpStatus.OK);
 			else
-				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-		}
-		
-		//ok
-		@RequestMapping(value = "/getProductById/{id}", method = RequestMethod.GET)
-		public ResponseEntity<ProductDetailDTO> getProductById(@PathVariable(name = "id") String id) {
-			ProductDetailDTO productDetailDTO= productService.getProductByIdUser(id);
-			if(productService.getProductById(id) != null) 
-				return new ResponseEntity<ProductDetailDTO>(productDetailDTO, HttpStatus.OK);
+				return new ResponseEntity<>(productList.subList(fromIndex, productList.size() - 1), HttpStatus.OK);
+		} else
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	}
+
+	@RequestMapping(value = "/m/listAllProductIncludeImage", method = RequestMethod.GET)
+	public ResponseEntity<?> listAllProductMobile(@RequestBody int fromIndex) {
+		List<ProductIncludeImageDTO> productList = productService.listAllProductIncludeImage();
+		int toIndex = fromIndex + 10;
+		if (!productList.isEmpty()) {
+			if (toIndex < productList.size())
+				return new ResponseEntity<>(productList.subList(fromIndex, toIndex), HttpStatus.OK);
 			else
-				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-		}
-		
+				return new ResponseEntity<>(productList.subList(fromIndex, productList.size() - 1), HttpStatus.OK);
+		} else
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	}
+
+	// ok
+	@RequestMapping(value = "/getProductById/{id}", method = RequestMethod.GET)
+	public ResponseEntity<ProductDetailDTO> getProductById(@PathVariable(name = "id") String id) {
+		ProductDetailDTO productDetailDTO = productService.getProductByIdUser(id);
+		if (productService.getProductById(id) != null)
+			return new ResponseEntity<ProductDetailDTO>(productDetailDTO, HttpStatus.OK);
+		else
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	}
+
 //		//ok
 //		@RequestMapping(value = "/getProductAllById/{id}", method = RequestMethod.GET)
 //		public ResponseEntity<ProductListDTO> getProductByIdAll(@PathVariable(name = "id") String id) {
-//			if(productService.getProductById(id) != null) 
+//			if(productService.getProductById(id) != null)
 //				return new ResponseEntity<>(productService.getProductByIdAdmin(id), HttpStatus.OK);
 //			else
 //				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
