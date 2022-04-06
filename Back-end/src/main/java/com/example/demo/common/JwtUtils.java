89261389d31e94442ec3dda6069913d39818285f
Hangvttse63286 <46786755+Hangvttse63286@users.noreply.github.com>
2022-04-06 16:40:21 +0700

    update cookie

--- Back-end/src/main/java/com/example/demo/common/JwtUtils.java
+++ Back-end/src/main/java/com/example/demo/common/JwtUtils.java
@@ -49,7 +49,7 @@ public class JwtUtils {
 
 	public ResponseCookie generateJwtCookie(UserDetailsImpl userPrincipal) {
 		String jwt = generateTokenFromUsername(userPrincipal.getUsername());
-		ResponseCookie cookie = ResponseCookie.from(jwtCookie, jwt).path("/api").maxAge(24 * 60 * 60).httpOnly(true)
+		ResponseCookie cookie = ResponseCookie.from(jwtCookie, jwt).path("/api").maxAge(24 * 60 * 60).httpOnly(true).sameSite("None")
 				.build();
 		return cookie;
 	}
