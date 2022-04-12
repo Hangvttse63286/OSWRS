9382de85584acee6beaaab94fb7f63b8509889b6
Hangvttse63286 <46786755+Hangvttse63286@users.noreply.github.com>
2022-04-13 01:43:49 +0700

    add allow header in cors

--- Back-end/src/main/java/com/example/demo/config/SecurityConfig.java
+++ Back-end/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -114,7 +114,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         configuration.addAllowedOrigin("http://localhost:8080");
         configuration.setAllowCredentials(true);
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
+        configuration.setAllowedHeaders(Arrays.asList("origin", "accept", "authorization", "content-type", "x-auth-token"));
         configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
