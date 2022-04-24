8608931f0a3a00aa9f5fc1d950266fac3e8537ba
Hangvttse63286 <46786755+Hangvttse63286@users.noreply.github.com>
2022-04-25 01:20:28 +0700

    add authorize header to swagger

--- Back-end/src/main/java/com/example/demo/config/SpringFoxConfig.java
+++ Back-end/src/main/java/com/example/demo/config/SpringFoxConfig.java
@@ -1,27 +1,52 @@
 package com.example.demo.config;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
 public class SpringFoxConfig {
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
+	@Bean
+	public Docket api() {
+	    return new Docket(DocumentationType.SWAGGER_2)
+	      .apiInfo(apiInfo())
+	      .securityContexts(Arrays.asList(securityContext()))
+	      .securitySchemes(Arrays.asList(apiKey()))
+	      .select()
+	      .apis(RequestHandlerSelectors.any())
+	      .paths(PathSelectors.any())
+	      .build();
+	}
+
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", "Authorization", "header");
     }
-    
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth()).build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
+    }
+
     private ApiInfo apiInfo() {
         ApiInfo apiInfo = new ApiInfo("My REST API", "Some custom description of API.", "API TOS", "Terms of service", "myeaddress@company.com", "License of API", "API license URL");
         return apiInfo;
